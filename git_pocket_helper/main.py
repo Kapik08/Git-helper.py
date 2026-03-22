365776e61a28ee144cbd1d37eb7bb3b2ed29e9f7
Kapik08 <zdrojewskkkacper@gmail.com>
2026-03-22 12:44:31 +0100

    bleh

--- git_pocket_helper/main.py
+++ git_pocket_helper/main.py
@@ -2,8 +2,43 @@ from lib.file_manager import *
 from lib.place_holder import *
 import pyautogui as pya
 import time
+
+
 isloop = True
 desiredPath = "gittemp"
+
+def logMenu():
+    clear()
+    
+    logFile(desiredPath,input('give me reason to:'))
+    print('writen file done')
+    time.sleep(2.3)
+
+
+
+def readMenu():
+    clear()
+
+    readFile(desiredPath)
+    input('press enter to go back  ')
+
+
+
+def gitHelper():
+    pass
+    print('1) set up git',end="")
+    print('       2) add && commit',"\n")
+
+
+    input('enter to close')
+    
+
+
+
+
+
+
+
 while isloop:
     clear()
     print('MENU 1. for xxx || 2. for yyy :  ',end="")
@@ -11,19 +46,17 @@ while isloop:
 
     match(inp):
         case 1:
-            clear()
-            
-            logFile(desiredPath,input('give me reason to:'))
-            print('writen file done')
-            time.sleep(2.3)
+            logMenu()
+
             
         case 2:
-            clear()
+            readMenu()
 
-            readFile(desiredPath)
-            input('press enter to go back  ')
-            
         case 3:
+            gitHelper()
+
+
+        case _:
             isloop = False
             
 
